5b01ff9a0e4659f7b0e7bc79c3b8260ae19f6acd
Tom-Håkon Fjeldheim <124530551+tomhaakon@users.noreply.github.com>
2026-08-05 21:25:09 +0200

    Refactor EditorSettings structure for improved readability and set fitGameView to true by default

--- include/teya/editor/EditorSettings.h
+++ include/teya/editor/EditorSettings.h
@@ -1,2 +1,9 @@
 #pragma once
-namespace teya::editor { struct EditorSettings { bool fitGameView = false; bool captureGameInput = false; bool showPlayerOrigin = false; bool showColliders = false; }; }
+namespace teya::editor {
+struct EditorSettings {
+    bool fitGameView = true;
+    bool captureGameInput = false;
+    bool showPlayerOrigin = false;
+    bool showColliders = false;
+};
+} // namespace teya::editor
